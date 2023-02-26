fe7c2bea5c37dd6e3c8371d24cb797187d3bfa15
asb19 <amirsohel.as10@gmail.com>
2023-02-26 17:03:29 +0530

    fix

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 const express = require('express')
 
+const AWS = require('aws-sdk');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
+    region: 'ap-south-1'
+});
+
 const app = express()
+
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req,res)=> {
-    res.render('index.html')
+    res.render('index', {
+        apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRET
+    })
 })
+
+app.post('/api/upload', (req, res) => {
+    const fileName = req.body.fileName;
+    const fileType = req.body.fileType;
+    const s3Params = {
+        Bucket: 'image-test-bucket1',
+        Key: fileName,
+        ContentType: fileType,
+        ACL: 'public-read'
+    };
+    s3.getSignedUrl('putObject', s3Params, (err, url) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: err });
+        } else {
+            res.json({ url: url });
+        }
+    });
+});
 
 
 
